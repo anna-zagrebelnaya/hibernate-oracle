13b94d363d68608612434444f3c486ace1b91f98
Anna <annzagrebelnaya@gmail.com>
2016-07-19 00:17:01 +0300

    add liquibase, generate initial schema

--- HibernateTest/src/main/java/com/anka/util/HibernateUtil.java
+++ HibernateTest/src/main/java/com/anka/util/HibernateUtil.java
@@ -3,16 +3,34 @@ package com.anka.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Properties;
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
+
+            Properties dbConnectionProperties = new Properties();
+            try {
+                dbConnectionProperties.load(HibernateUtil.class.getClassLoader().getSystemClassLoader()
+                        .getResourceAsStream("db.properties"));
+            } catch(Exception e) {
+                System.err.println("Hibernate properties loading failed." + e);
+                throw new ExceptionInInitializerError(e);
+            }
+
+            return new Configuration()
+                    .setProperty("hibernate.connection.url", dbConnectionProperties.getProperty("url"))
+                    .setProperty("hibernate.connection.driver_class", dbConnectionProperties.getProperty("driver"))
+                    .setProperty("hibernate.connection.username", dbConnectionProperties.getProperty("username"))
+                    .setProperty("hibernate.connection.password", dbConnectionProperties.getProperty("password"))
+                    .setProperty("hibernate.default_schema", dbConnectionProperties.getProperty("default_schema"))
+                    .configure("hibernate.cfg.xml").buildSessionFactory();
+
+
         } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
